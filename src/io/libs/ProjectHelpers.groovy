7f6870613ce842f9cc0dfb9c7c817e0c08e0b58a
FNS Reports <KrasilnikovMihail@gmail.com>
2019-06-28 10:50:48 +0300

    errors

--- src/io/libs/ProjectHelpers.groovy
+++ src/io/libs/ProjectHelpers.groovy
@@ -10,7 +10,7 @@ package io.libs
 //  cfdt - файловый путь к dt или cf конфигурации для загрузки. Только для пакетного режима!
 //  isras - если true, то используется RAS для скрипта, в противном случае - пакетный режим
 //
-def createDb(platform, server1c, serversql, base, cfdt, isras, sqluser="", sqlPwd="") {
+def createDb(platform, server1c, serversql, base, cfdt, isras, sqluser="aaa", sqlPwd="ppp") {
     utils = new Utils()
 
     cfdtpath = ""
